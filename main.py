e0e95754ad63ae0a4e4c457bead9225dcbc8a050
Frederick Engelns-Bauer <126475631+fengelnsbauer@users.noreply.github.com>
2025-04-14 00:41:39 +0200

    Fe/add configuration backend (#11)
    * Initial Commit
    
    * Add CORS middleware and configuration endpoint
    
    * Match with frontend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from faststream.nats.fastapi import NatsRouter
 
 from clients import milvus, tinydb
@@ -10,6 +11,11 @@ from config.base import settings
 
 router = NatsRouter(settings.nats.url)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
 
 @router.get("/")
 async def hello_http():
@@ -26,12 +32,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(routers.simulation.router)
 app.include_router(routers.world.router)
 app.include_router(routers.agent.router)
 app.include_router(routers.debug.router)
+app.include_router(routers.configuration.router)
 
 # Include subscribers
 app.include_router(subscribers.world.router)
