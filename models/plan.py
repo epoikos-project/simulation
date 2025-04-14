03830988423899c9cafb115620a67fc7f4f564ae
benediktpri <felix.2001@web.de>
2025-04-14 23:17:03 +0200

    refactor plan

--- models/plan.py
+++ models/plan.py
@@ -1,80 +1,89 @@
+import uuid
+
 from loguru import logger
 from tinydb import TinyDB
 from config.base import settings
 from clients.nats import NatsBroker
+from tinydb import Query
 
 
 class Plan:
-    def __init__(self, db: TinyDB, nats: NatsBroker, id: str):
-        self.id: str = id
-        self.simulation_id: str
-        self._db = db
-        self._nats = nats
-        self.participants: list = []
-        self.goal: str
-        self.tasks: list = []  # TODO: consider if list suffices
-        self.total_payoff: int = 0
+    def __init__(self, db: TinyDB, nats: NatsBroker, id: str, simulation_id: str):
+        if id is None:
+            self.id = uuid.uuid4().hex
+        else:
+            self.id = id
+        self.simulation_id: str = simulation_id
+        self._db: TinyDB = db
+        self._nats: NatsBroker = nats
+        self.owner: str | None = None  # agent.id
+        self.participants: list[str] = []  # [agent.id]
+        self.tasks: list[str] = []  # [task.id]
+        self.goal: str | None = None
+        self.total_payoff: int = self._calculate_expected_payoff()
 
     def __repr__(self) -> str:
         return f"Plan(id={self.id}, simulation_id={self.simulation_id})"
 
-    def _create_in_db(self):
-        table = self._db.table(settings.tinydb.tables.plan_table)
-        table.insert(
-            {
-                "id": self.id,
-                "simulation_id": self.simulation_id,
-                "owner": None,  # Agent class
-                "participants": [],
-                "tasks": [],
-                "goal": str,  # is this just a description? relation to tasks?
-                "total_expected_payoff": 0,  # Total expected payoff for the plan
-            }
-        )
+    def _get_plan_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "simulation_id": self.simulation_id,
+            "owner": self.owner,
+            "participants": self.participants,
+            "tasks": self.tasks,
+            "goal": self.goal,
+            "total_expected_payoff": self._calculate_expected_payoff(),
+        }
 
-    # interaction with nats stream? --> do all actions need to be logged? Yes. But done in router?
-    # interaction with milvus? --> not needed
+    def _save_to_db(self):
+        table = self._db.table(settings.tinydb.tables.plan_table)
+        plan = self._get_plan_dict()
+        table.upsert(plan, Query().id == self.id)
+
+    def _calculate_expected_payoff(self) -> int:
+        """Return the total expected payoff for the plan."""
+
+        tasks = self._db.table(settings.tinydb.tables.task_table).search(
+            Query().id.one_of(self.tasks)
+        )
+        # tasks = [Task(**task) for task in tasks]
+        total_payoff = sum(task["payoff"] for task in tasks)
+        return total_payoff
 
     async def create(self):
-        logger.info(f"Creating Plan {self.id}")
-        self._create_in_db()
+        logger.info(f"Creating plan {self.id}")
+        self._save_to_db()
 
-    def add_task(self, task):
+    def add_task(self, task_id: str):
         """Add a task to the plan."""
-        self.tasks.append(task)
-        self.total_payoff += task.payoff
-        # TODO: insert task into db
+        self.tasks.append(task_id)
+        self._save_to_db()
 
-    def remove_task(self, task):
+    def remove_task(self, task_id: str):
         """Remove a task from the plan."""
-        if task in self.tasks:
-            self.tasks.remove(task)
-            self.total_payoff -= task.payoff
+        if task_id in self.tasks:
+            self.tasks.remove(task_id)
+            self._save_to_db()
         else:
-            logger.warning(f"Task {task} not found in plan {self.id}")
+            logger.warning(f"Task {task_id} not found in plan {self.id}")
 
-    def add_participant(self, participant):
+    def add_participant(self, agent_id: str):
         """Add a participant to the plan."""
-        self.participants.append(participant)
+        self.participants.append(agent_id)
 
-    def remove_participant(self, participant):
+    def remove_participant(self, agent_id):
         """Remove a participant from the plan."""
-        if participant in self.participants:
-            self.participants.remove(participant)
+        if agent_id in self.participants:
+            self.participants.remove(agent_id)
         else:
-            logger.warning(f"Participant {participant} not found in plan {self.id}")
+            logger.warning(f"Participant {agent_id} not found in plan {self.id}")
 
-    def updated_expected_payoff(self):
-        """Update the expected payoff for the plan."""
-        self.total_payoff = sum(task.payoff for task in self.tasks)
-
-    def get_participants(self):
+    def get_participants(self) -> list[str]:
         """Get the participants of the plan."""
         return self.participants
-        # TODO: could also query this from db in router
 
-    # execution of plan performed by agent, but updates of plan here
-    # need to consider relation to tasks
-    # what functionality implemented here vs. directly querying in router?
-    # why do we need db here? I see that it is probably a good idea to have but would work without as well I guess
-    # everything performed twice, once in db and once in plan
+    def pass_ownership(self, agent_id: str):
+        """Pass ownership of the plan to a new agent."""
+        self.owner = agent_id
+        self._save_to_db()
