548ae7fe91fb0309d390a758cc83cea95b881a90
Yannik Hahn <yannik@h4hn.de>
2025-04-22 19:51:02 +0200

    add convo tool (#28)
    Co-authored-by: Frederick Engelns-Bauer <frederick.engelns-bauer@sap.com>

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,12 @@
 from .plan_tools import make_plan, add_task, take_on_task
 from .world_tools import move
+from .conversation_tools import start_conversation, engage_conversation
 
-available_tools = [move, make_plan, add_task, take_on_task]
+available_tools = [
+    move,
+    make_plan,
+    add_task,
+    take_on_task,
+    start_conversation,
+    engage_conversation,
+]
