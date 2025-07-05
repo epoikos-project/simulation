c909ef7f1d46cdc8b916edbaaeba92ad2a50242d
Yannik Hahn <yannik@h4hn.de>
2025-07-05 16:51:39 +0200

    fix: sqlalchemy migrations from env

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
+from config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -54,7 +56,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = settings.db.url
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -74,7 +76,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration={"sqlalchemy.url": settings.db.url},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
